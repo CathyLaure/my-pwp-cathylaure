7e098b27b47f8f07d4323a70667e40c8911244a8
Cathy Tasama <ctasama@cnm.edu>
2020-03-24 13:10:53 -0600

    Took out <ul> and replaced with <p>

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html langa="en">
+<html lang="en">
 <head>
 	<meta charset="UTF-8" />
 	<title>Milestone 1</title>
@@ -7,7 +7,7 @@
 </head>
 <body>
 <h1>Purpose, Audience and Goal</h1>
-	<h2>Purpose</h2>
+	<h2><strong>Purpose</strong></h2>
 		<div class="center">
 			<ul> This is a personal portfolio website. It is meant to showcase my work as a web developer.
 			</ul>
@@ -20,47 +20,45 @@
 			</ul>
 		</div>
 	<h2>Goal</h2>
-		<div class="center">
-			<ul>The primary goal of this site is for users of the site to initiate contact with me in regards
+			<p>The primary goal of this site is for users of the site to initiate contact with me in regards
 				to employment and collaboration opportunities through the contact form, and/or the connected
 				social media profiles. The secondary goal of this site is to showcase my work,
 				and to contribute to a polished professional online presence.
-			</ul>
-		</div>
+			</p>
 	<h1>Persona</h1>
-		<img src="AmyKao.PNG" alt="Picture of Cathy" id="AmyKoa";
+		<img src="AmyKao.PNG" alt="Picture of Cathy" id="AmyKoa"
 	 		 class="center"/>
-		<ul>Name: Amy Koa</ul>
-		<ul>Age: 38</ul>
-		<ul>Profession: Amy Kao, the co-founder and CMO of RxAll looking to hire a full-time Web Developer.
+		<p><strong>Name</strong>: Amy Koa</p>
+		<p><strong>Age</strong>: 38</p>
+		<p><strong>Profession</strong>: Amy Kao, the co-founder and CMO of RxAll looking to hire a full-time Web Developer.
 			RxAll is a deep learning hyperspectral IoT platform for authenticating drugs.
 			The platform uses a proprietary molecular sensor device with a cloud-based IP-protected deep
 			learning algorithm and database of spectral signatures of drugs (both prescription and recreational)
 			to carry out non-destructive drug authentication.
-		</ul>
-		<ul>Technology:  Mac power user. Uses a Mac Pro Tower with 2 30” cinema displays while in the office, a MacBook
+		</p>
+		<p><strong>Technology</strong>:  Mac power user. Uses a Mac Pro Tower with 2 30” cinema displays while in the office, a MacBook
 			Pro laptop at home, and an iPhone 11  with an unlimited 4G data connection.
-		</ul>
-		<ul>Attitudes and Behaviors: Very busy entrepreneur having less than 5 minutes to spend viewing a
+		</p>
+		<p><strong>Attitudes and Behaviors</strong>: Very busy entrepreneur having less than 5 minutes to spend viewing a
 			portfolio website. Amy is a new social media user seeking potential candidates through tech and
 			medecine community networking events, LinkedIn, Facebook and Twitter.
-		</ul>
-		<ul>Frustrations and Needs: RxAll is in need of better online presence and visibility.
+		</p>
+		<p><strong>Frustrations and Needs</strong>: RxAll is in need of better online presence and visibility.
 			Amy needs to hire someone with competence and development experience who is willing
 			to start at a competitive entry-level salary, to start. Amy is also looking for someone with a
 			good personality and positive mindset. Because Amy is looking to hire within a very competitive
 			market, she is flooded with candidates but is having a hard time finding someone with the
 			right combination of talent, experience, and personality who is willing to accept a slightly lower
 			salary and limited benefits. She must hire within 1 month.
-		</ul>
-		<ul>Goals: Amy is mainly concerned with one thing; is this person a potential candidate for the RxAll
+		</p>
+		<p><strong>Goals</strong>: Amy is mainly concerned with one thing; is this person a potential candidate for the RxAll
 			Web Dev position? Can Amy, within 5 minutes max, determine whether this person is worthy of a preliminary
 			phone screen regarding the job? Amy doesn't want to waste time. Amy needs to know via a very fast
 			interaction with the site whether or not this potential candidate has the minimum qualifications to
 			warrant a call.
-		</ul>
-		<ul>User Story: As a user, I need to determine quickly whether this person is a good candidate for the entry
+		</p>
+		<p><strong>User Story</strong>: As a user, I need to determine quickly whether this person is a good candidate for the entry
 		level Web Developer position.
-		</ul>
+		</p>
 </body>
 </html>
