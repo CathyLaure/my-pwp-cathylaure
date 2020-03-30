f98a550732339c12b5a570a0d98ea0dea249ae4b
Cathy Tasama <ctasama@cnm.edu>
2020-03-29 23:02:35 -0600

    Wrote the content strategy for my website

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-pwp-cathylaure
-Personal Website Project.
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone 2</title>
+</head>
+<body>
+<h1>Content Strategy</h1>
+	<p>My site will be on a single page consisting of the following sections:</p>
+	<p><strong>Welcome</strong>: A brief introduction of myself on a background of a picture
+	of me in an office setup</p>
+	<p>
+		<strong>About Me</strong>: This section will include:</p>
+		<ul>
+			<li>Fun facts about me.</li>
+			<li>My strengths and my personality.</li>
+			<li>Core skills I possess</li>
+			<li>Some pictures of me working/ coding</li>
+		</ul>
+	<p><strong>Portfolio</strong>: This section will include the projects I have worked on
+	specifying my role in the projects. It may also include the side projects I have worked
+	on and am willing to share with the public.</p>
+	<p><strong>Contact</strong>: This section will contain a simple contact form,
+		my professional email address, and additional links to my professional social media profiles:
+		LinkedIn, Twitter, GitHub,
+		(These will also be linked to in the footer site wide via icons.)</p>
+</body>
+</html>
